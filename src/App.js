ee0dc24f407beecbf4862088288523ebe3ec2aef
Patt Höge <paty.hoge98@gmail.com>
2024-01-22 17:03:08 +0000

    React Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Button from "react-bootstrap/Button";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>
-        Moments
+      <Button variant="primary">Moments</Button>
       </h1>
     </div>
   );
