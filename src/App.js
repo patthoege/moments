577f6a35fce4abf89f94c3c0dc8299f9321fee45
Patt Höge <paty.hoge98@gmail.com>
2024-01-23 11:30:28 +0000

    creating NavBar component and adding logo and css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import Button from "react-bootstrap/Button";
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>
-      <Button variant="primary">Moments</Button>
-      </h1>
+      <NavBar />
     </div>
   );
 }
