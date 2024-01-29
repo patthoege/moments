3840f0b9ea3629802bc166958de69e8917b6f43a
Patt Höge <paty.hoge98@gmail.com>
2024-01-29 20:32:08 +0000

    Creating Posts Part Two - Sending data to the API

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://drfi-api-a41e37271e42.herokuapp.com";
+axios.defaults.baseURL = "https://drfi-api-a41e37271e42.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
