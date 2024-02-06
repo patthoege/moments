663f87310f86157932607460be8d6b8e2e24b565
Patt Höge <paty.hoge98@gmail.com>
2024-02-06 14:10:43 +0000

    Testing in React - part 2

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,7 +2,7 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
 import { setupServer } from "msw/node";
 import { handlers } from "./mocks/handlers";
 
