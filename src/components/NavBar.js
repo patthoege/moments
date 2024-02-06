68bd860430ee4f19e32156e6b374ab84c954a1b0
Patt Höge <paty.hoge98@gmail.com>
2024-02-06 16:19:49 +0000

    cleaning up errors

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,6 +10,8 @@ import {
 import Avatar from "./Avatar";
 import axios from "axios";
 import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
+import { removeTokenTimestamp } from "../utils/utils";
+
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
@@ -21,6 +23,7 @@ const NavBar = () => {
     try {
       await axios.post("dj-rest-auth/logout/");
       setCurrentUser(null);
+      removeTokenTimestamp();
     } catch (err) {
       console.log(err);
     }
